6d5ff92cf3f21db973bcca1c9ee43c3ce05f3da2
GMounika657 <140373860+GMounika657@users.noreply.github.com>
2023-10-30 12:12:09 +0530

    Update Welcome.java

--- Demopush/src/pack1/Welcome.java
+++ Demopush/src/pack1/Welcome.java
@@ -4,7 +4,8 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java");
-		System.out.println("Hello world");// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Hello");// TODO Auto-generated method stub
 
 	}
 
